ca2163dc2b76fa5fb40c6f6ec30d96d6cc0d510b
jewelrana3 <juyelrana7752@gmail.com>
2023-04-07 23:14:27 +0600

    problem solve

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 
-const Cart = ({cart,removeAdd}) => {
- 
+const Cart = ({ cart, removeAdd }) => {
+   
+    let message;
+    if (cart.length === 0) {
+        message = <p>Pelase add a product</p>
+    }
+
+
     return (
         <div>
             <h4>Order Summary :{cart.length}</h4>
-           
+            {message}
             {
-              cart.map(tshirt=><p 
-                key={tshirt._id}>
-                {tshirt.name}
-                <button onClick={()=>removeAdd(tshirt._id)}>x</button>
+                cart.map(tshirt => <p
+                    key={tshirt._id}>
+                    {tshirt.name}
+                    <button onClick={() => removeAdd(tshirt._id)}>x</button>
                 </p>)
             }
         </div>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,25 +3,40 @@ import { useLoaderData } from 'react-router-dom';
 import Tshirt from '../Tshirt/Tshirt';
 import Cart from '../Cart/Cart';
 import './Home.css'
+import toast from 'react-hot-toast';
 
 const Home = () => {
     const shirts = useLoaderData();
-    const [cart,setCart] = useState([]);
+    const [cart, setCart] = useState([]);
 
-    const handleAdd = shirt =>{
-        const newCart = [...cart,shirt]
-        setCart(newCart)
+    const handleAdd = shirt => {
+        // console.log(shirt)
+        const exits = cart.find(ts => ts._id === shirt._id)
+        console.log({exits,shirt,cart})
+        if (exits) {
+            toast('already add ')
+        }
+        else {
+            const newCart = [...cart, shirt]
+            setCart(newCart)
+
+
+        }
+
     }
 
-    const removeAdd=id=>{
+    const removeAdd = id => {
         console.log(id)
+        const remaing = cart.filter(ts => ts._id !== id);
+        console.log({cart,remaing})
+        setCart(remaing)
     }
     return (
         <div className='home-container'>
             <div className='shirt-container'>
                 {
                     shirts.map(shirt => <Tshirt
-                        key={shirt.id}
+                        key={shirt._id}
                         shirt={shirt}
                         handleAdd={handleAdd}
                     ></Tshirt>)
@@ -29,7 +44,7 @@ const Home = () => {
             </div>
             <div className="cart-container">
                 <Cart cart={cart}
-                removeAdd={removeAdd}
+                    removeAdd={removeAdd}
                 ></Cart>
             </div>
         </div>
